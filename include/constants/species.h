5f473c74570d6c3b6d87211be13c319853b08382
haxboy <owenbfinney@gmail.com>
2025-01-10 01:17:17 -0800

    SKERRIBLE IN DEX (NO SPRITE)
    SKERRIBLE EXISTS, HAS CRY, HAS SOME INFO, NEEDS SPRITE, NEED TO LOOK CLOSER AT DEXES

--- include/constants/species.h
+++ include/constants/species.h
@@ -1637,9 +1637,9 @@
 #define SPECIES_URSHIFU_RAPID_STRIKE_STYLE_GMAX         SPECIES_URSHIFU_RAPID_STRIKE_GMAX
 #define SPECIES_MIMIKYU_BUSTED_TOTEM                    1523
 #define SPECIES_MIMIKYU_TOTEM_BUSTED                    SPECIES_MIMIKYU_BUSTED_TOTEM
-#define SPECIES_CHLOROSNOW                              1524
+#define SPECIES_SKERRIBBLE                              1524
 
-#define SPECIES_EGG                                     (SPECIES_CHLOROSNOW + 1)
+#define SPECIES_EGG                                     (SPECIES_SKERRIBBLE + 1)
 
 #define NUM_SPECIES SPECIES_EGG
 
